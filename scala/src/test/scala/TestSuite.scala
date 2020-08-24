f09663d6f0f2c09383a3e043a6234961fbd1d060
bhavik <bhavik@email.com>
2020-08-24 08:23:28 -0500

    added more tests for language feature tests

--- scala/src/test/scala/TestSuite.scala
+++ scala/src/test/scala/TestSuite.scala
@@ -393,6 +393,48 @@ class TestSuite extends AnyFunSuite {
 
   test("Collections: Transformation") {
     // operations for converting from one type of collection to the other
+    case class Event(location: String, dayOfWeek: String, sessionTimeInSeconds: Int, source: String)
 
+    val event1 = Event("US", "Sunday", 10, "twitter")
+    val event2 = Event("Japan", "Monday", 10, "twitter")
+    val event3 = Event("England", "Sunday", 10, "twitter")
+    val event4 = Event("Philippines", "Monday", 10, "twitter")
+    val event5 = Event("Mexico", "Sunday", 10, "twitter")
+
+    val events = List(event1, event2, event3, event4, event5)
+    //    val locations = events.map(eventItem => eventItem.location)
+    val locations = events.map(_.location)
+
+    assert(locations.isInstanceOf[List[String]], true)
+    assert(locations.size == events.size, true)
+  }
+
+  test("Collections: Flatmap") {
+    val list = List(List(1, 2, 3, 4), List(10, 21, 32, 43))
+    val singleList = list.map(list => list.map(_ + 1)).flatten
+    val singleListWithFlatMap = list.flatMap(list => list.map(_ + 1))
+
+    assert(singleList == List(2, 3, 4, 5, 11, 22, 33, 44), true)
+    assert(singleListWithFlatMap == List(2, 3, 4, 5, 11, 22, 33, 44), true)
+  }
+
+  test("Collections: Flatmap and Option") {
+    def convertToInt(s: String): Option[Int] = {
+      try {
+        Some(s.toInt)
+      } catch {
+        case e: NumberFormatException => None
+      }
+    }
+
+    val arguments = List("10", "eight", "5")
+
+    assert(arguments.map(num => convertToInt(num)) == List(Some(10), None, Some(5)), true)
+    assert(arguments.map(convertToInt(_)) == List(Some(10), None, Some(5)), true)
+    assert(arguments.map(convertToInt) == List(Some(10), None, Some(5)), true)
+
+    assert(arguments.flatMap(convertToInt) == List(10, 5), true)
+    assert(arguments.map(convertToInt).flatten.sum == 15, true)
+    assert(arguments.flatMap(convertToInt).sum == 15, true)
   }
 }
