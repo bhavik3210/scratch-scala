21b379f578d278e19b80f4e3b78f187644e5ee2a
bhavik <bhavik@email.com>
2020-10-14 10:00:10 -0500

    method notations

--- rock-the-jvm/rtj-scala-beginners/src/lectures/part2oop/MethodNotations.scala
+++ rock-the-jvm/rtj-scala-beginners/src/lectures/part2oop/MethodNotations.scala
@@ -1,19 +1,31 @@
 package lectures.part2oop
 
+import scala.language.postfixOps
+
 object MethodNotations extends App {
 
-  class Person(val name: String, favoriteMovie: String) {
+  class Person(val name: String, favoriteMovie: String, val age: Int = 0) {
     def likes(movie: String): Boolean = movie == favoriteMovie
 
     def hangoutWith(person: Person): String = s"${this.name} is hanging out with ${person.name}"
 
     def +(person: Person): String = s"${this.name} ${person.name} are together." // infix notation
 
+    def +(nickname: String): Person = new Person(s"$name ($nickname)", favoriteMovie)
+
     def unary_! : String = s"$name, what the heck?" //prefix notation
+
+    def unary_+ : Person = new Person(name, favoriteMovie, age + 1)
 
     def isAlive: Boolean = true //postfix notation
 
+    def learns(value: String): String = s"$name learns $value"
+
+    def learnScala: String = this learns "Scala"
+
     def apply(): String = s"Hi my name is $name and I Like $favoriteMovie"
+
+    def apply(numberOfTimesWatchedMovie: Int): String = s"$name watched $favoriteMovie $numberOfTimesWatchedMovie time(s)."
   }
 
   val mary = new Person("Mary", "Interstellar")
@@ -50,4 +62,13 @@ object MethodNotations extends App {
   println(mary.apply())
   println(mary()) // equivalent to .apply() above
 
+
+  /*
+  Exercises
+   */
+  val rockstarMary = mary + "the rockstar"
+  println(rockstarMary.apply())
+  println(+mary age)
+  println(mary learnScala)
+  println(mary.apply(3))
 }
