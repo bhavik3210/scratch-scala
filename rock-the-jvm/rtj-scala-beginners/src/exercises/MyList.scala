d32c3df711daa67cfef0f292f1e9dfcb6ad23ec9
Bhavik Patel <bhavik.patel@rallyhealth.com>
2020-12-09 17:58:40 -0600

    updated list

--- rock-the-jvm/rtj-scala-beginners/src/exercises/MyList.scala
+++ rock-the-jvm/rtj-scala-beginners/src/exercises/MyList.scala
@@ -6,16 +6,23 @@ abstract class MyList[+A] {
 
   def isEmpty: Boolean
 
-  // handling covariance situation
-  def add[B >: A](element: B): MyList[B]
+  def add[B >: A](element: B): MyList[B] // handling covariance situation
 
   def printElements: String
 
   override def toString: String = "[" + printElements + "]"
+
+  def map[B](transformer: MyTransformer[A, B]): MyList[B]
+
+  def flatMap[B](transformer: MyTransformer[A, MyList[B]]): MyList[B]
+
+  def filter(predicate: MyPredicate[A]): MyList[A]
+
+  def ++[B >: A](list: MyList[B]): MyList[B]
 }
 
 
-object Empty extends MyList[Nothing] {
+case object Empty extends MyList[Nothing] {
   val head: Nothing = throw new NoSuchElementException
   val tail: MyList[Nothing] = throw new NoSuchElementException
 
@@ -24,6 +31,14 @@ object Empty extends MyList[Nothing] {
   def add[B >: Nothing](element: B): MyList[B] = new Cons(element, Empty)
 
   override def printElements: String = ""
+
+  def map[B](transformer: MyTransformer[Nothing, B]): MyList[B] = Empty
+
+  def flatMap[B](transformer: MyTransformer[Nothing, MyList[B]]): MyList[B] = Empty
+
+  def filter(predicate: MyPredicate[Nothing]): MyList[Nothing] = Empty
+
+  def ++[B >: Nothing](list: MyList[B]): MyList[B] = list
 }
 
 class Cons[+A](h: A, t: MyList[A]) extends MyList[A] {
@@ -37,17 +52,71 @@ class Cons[+A](h: A, t: MyList[A]) extends MyList[A] {
   override def printElements: String =
     if (t.isEmpty) "" + head
     else h + " " + t.printElements
+
+  /*
+   [1,2,3].filter(n % 2 == 0) =
+     [2,3].filter(n % 2 == 0) =
+     = new Cons(2, [3].filter(n % 2 == 0))
+     = new Cons(2, Empty.filter(n % 2 == 0))
+     = new Cons(2, Empty)
+   */
+  def filter(predicate: MyPredicate[A]): MyList[A] =
+    if (predicate.test(h)) new Cons(h, t.filter(predicate))
+    else t.filter(predicate)
+
+
+  /*
+    [1,2,3].map(n * 2)
+      = new Cons(2, [2,3].map(n * 2))
+      = new Cons(2, new Cons(4, [3].map(n * 2)))
+      = new Cons(2, new Cons(4, new Cons(6, Empty.map(n * 2))))
+      = new Cons(2, new Cons(4, new Cons(6, Empty))))
+   */
+  def map[B](transformer: MyTransformer[A, B]): MyList[B] =
+    new Cons(transformer.transform(h), t.map(transformer))
+
+  // concatenation
+  /*
+    [1, 2] ++ [3,4,5]
+    = new Cons(1, [2] ++ [3,4,5])
+    = new Cons(1, new Cons(2, Empty ++ [3,4,5]))
+    = new Cons(1, new Cons(2, new Cons(3, new Cons(4, new Cons(5)))))
+   */
+  def ++[B >: A](list: MyList[B]): MyList[B] = new Cons(h, t ++ list)
+
+  /*
+    [1,2].flatmap(n => [n, n+1])
+    = [1,2] ++ [2].flatMap(n => [n, n+1])
+    = [1,2] ++ [2,3] ++ Empty.flatMap(n => [n, n+1])
+    = [1,2] ++ [2,3] ++ Empty
+    = [1,2,2,3]
+   */
+  def flatMap[B](transformer: MyTransformer[A, MyList[B]]): MyList[B] =
+    transformer.transform(h) ++ t.flatMap(transformer)
+
+}
+
+trait MyPredicate[-T] {
+  def test(elem: T): Boolean
+}
+
+trait MyTransformer[-A, B] {
+  def transform(elem: A): B
 }
 
 object ListTest extends App {
-  val list = new Cons(1, new Cons(2, new Cons(3, Empty)))
-  println(list.tail.head)
-  println(list.add(4).head)
-  println(list.toString)
+  val listOfIntegers: MyList[Int] = new Cons(1, new Cons(2, new Cons(3, Empty)))
+  val cloneListOfIntegers: MyList[Int] = new Cons(1, new Cons(2, new Cons(3, Empty)))
+  val anotherListOfIntegers: MyList[Int] = new Cons(4, new Cons(5, Empty))
+  val listOfStrings: MyList[String] = new Cons("Hello", new Cons("Scala", Empty))
+
+  //  println(list.tail.head)
+  //  println(list.add(4).head)
+  //  println(list.toString)
   //
-  //  val listOfIntegers: MyList[Int] = new Cons(1, new Cons(2, new Cons(3, Empty)))
-  //  val listOfStrings: MyList[String] = new Cons("Hello", new Cons(",", new Cons("World", Empty)))
+  //    val listOfIntegers: MyList[Int] = new Cons(1, new Cons(2, new Cons(3, Empty)))
+  //    val listOfStrings: MyList[String] = new Cons("Hello", new Cons(",", new Cons("World", Empty)))
   //
-  //  println(listOfIntegers.toString)
-  //  println(listOfStrings.toString)
+  //    println(listOfIntegers.toString)
+  //    println(listOfStrings.toString)
 }
